128db745b53eaf84c3378d40feb0b45bec717d06
Christian-Hughes <chugh49@lsu.edu>
2021-02-18 17:59:17 -0500

    congiguring for heroku

--- tweeter_app/settings.py
+++ tweeter_app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4', #new
     'users', #new
     'tweets', #new
 ]
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'tweeter_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [(os.path.join(BASE_DIR, 'templates')), ], # new
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
